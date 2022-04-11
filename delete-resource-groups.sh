5287e5ad70f7cb1312bc18c88cd56edd52e2cbc5
hiryamada <71864722+hiryamada@users.noreply.github.com>
2022-04-11 21:01:55 +0900

    update

--- delete-resource-groups.sh
+++ delete-resource-groups.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# delete all resource groups, except cloud-shell's
 az group list --query '[].name' --output tsv |grep -v cloud-shell-storage |xargs -P10 -r -tl az group delete --no-wait --yes -g
